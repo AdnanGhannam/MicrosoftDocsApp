910418d05706856972aa4fb7b355bcfa4be549b4
Adnan <adnanghannam21@gmail.com>
2022-04-12 19:28:38 +0300

    added XamarinPage

---
+++ src/MicrosoftDocs.Client/Models/PageHeaderItemModel.cs
@@ -0,0 +1,8 @@
+﻿using MudBlazor;
+
+namespace MicrosoftDocs.Client.Models;
+
+public record PageHeaderItemModel(string Label, 
+    string Icon, 
+    Dictionary<string, string>? Items = null, 
+    string? Url = null);
