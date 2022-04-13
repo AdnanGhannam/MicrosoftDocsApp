a45e51697c8ec328ad16c03f60bda1d346dced3f
Adnan <adnanghannam21@gmail.com>
2022-04-14 00:03:22 +0300

    Fixed Menu.razor

---
+++ src/MicrosoftDocs.Client/wwwroot/scripts/main.js
@@ -0,0 +1,10 @@
+﻿
+function addEventToWindow(id) {
+    window.addEventListener('mouseup', function (event) {
+        let menu = document.querySelector('[data-id="' + id + '"]');
+        var box = document.getElementById("menu-cb-" + id);
+        if (menu && box && !menu.contains(event.target)) {
+            box.checked = false;
+        }
+    });
+}
