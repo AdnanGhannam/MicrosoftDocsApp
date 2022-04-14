ed1450a46b4ecd1574b287592db63985f9a9ee8e
Adnan <adnanghannam21@gmail.com>
2022-04-14 22:53:32 +0300

    Add media-query

--- src/MicrosoftDocs.Client/wwwroot/scripts/main.js
+++ src/MicrosoftDocs.Client/wwwroot/scripts/main.js
@@ -8,6 +8,26 @@
     });
 }
 
+function checkOverflow(selector) {
+    let el = document.querySelector(selector);
+
+    if (el) {
+        let curOverflow = el.style.overflow;
+
+        if (!curOverflow || curOverflow === "visible")
+            el.style.overflow = "hidden";
+
+        let isOverflowing = el.clientWidth < el.scrollWidth
+            || el.clientHeight < el.scrollHeight;
+
+        el.style.overflow = curOverflow;
+
+        return isOverflowing;
+    }
+
+    return false;
+}
+
 function handleClickOutsideMenu(id) {
     handleOnBlur('[data-id="' + id + '"]', () => {
         let checkbox = document.getElementById("menu-cb-" + id);
