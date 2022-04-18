120bfdd8c81bbe35716aeaa2044426a289bd3b8e
Adnan <adnanghannam21@gmail.com>
2022-04-18 21:30:01 +0300

    add ProductsController.cs

---
+++ src/MicrosoftDocs.Shared/ControllersRoutes/ProductsControllerRoutes.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MicrosoftDocs.Shared.ControllersRoutes;
+
+public static class ProductsControllerRoutes
+{
+    public const string Root = "/api/[controller]";
+    public const string GetAll = "";
+}
---
+++ src/MicrosoftDocs.Web/Controllers/ProductsController.cs
@@ -0,0 +1,31 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using MicrosoftDocs.Infrastructure.Data;
+using MicrosoftDocs.Shared.ControllersRoutes;
+
+namespace MicrosoftDocs.Web.Controllers;
+
+[ApiController]
+[Route(ProductsControllerRoutes.Root)]
+[Authorize]
+public class ProductsController : Controller
+{
+    private readonly IMediator _mediator;
+    private readonly AppDbContext context;
+
+    public ProductsController(IMediator mediator,
+        AppDbContext context)
+    {
+        _mediator = mediator;
+        this.context = context;
+    }
+
+    [AllowAnonymous]
+    [HttpGet(ProductsControllerRoutes.GetAll)]
+
+    public async Task<IActionResult> Index()
+    {
+        return Ok();
+    }
+}
